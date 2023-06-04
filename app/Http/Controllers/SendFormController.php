6e12f406c7e1154dcc018fbf11c273510ee7b2af
ludoguenet <ludovicguenet@gmx.com>
2023-06-04 14:53:51 +0200

    feat: added filepond

--- app/Http/Controllers/SendFormController.php
+++ app/Http/Controllers/SendFormController.php
@@ -4,15 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use RahulHaque\Filepond\Facades\Filepond;
 
 class SendFormController extends Controller
 {
     public function __invoke(
         Request $request
     ): JsonResponse {
+        $request->validate([
+           'gallery.*' => Rule::filepond([
+               'required',
+               'image',
+               'max:2000',
+           ]),
+        ]);
+
+        $galleryName = 'gallery-' . 1;
+
+        $fileInfos = Filepond::field($request->gallery)
+            ->moveTo('galleries/' . $galleryName);
 
         return response()->json([
             'success' => true,
+            'data' => $fileInfos,
         ]);
     }
 }
--- resources/js/components/FilepondForm.vue
+++ resources/js/components/FilepondForm.vue
@@ -1,9 +1,41 @@
 <script setup>
 import {ref} from "vue";
+import vueFilePond, { setOptions } from "vue-filepond";
+import "filepond/dist/filepond.min.css";
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css";
+import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
 import axios from "axios";
 
+const FilePond = vueFilePond(
+    FilePondPluginFileValidateType,
+    FilePondPluginImagePreview
+);
+
+const gallery = ref([]);
+
+const handleFilePondInit = () => {
+    console.log("FilePond has initialized");
+
+    setOptions({
+        server: {
+            url: '/filepond',
+            headers: {
+                'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+            },
+        },
+    });
+}
+
+const handleProcessFile = (error, file) => {
+    gallery.value.push(file.serverId);
+    console.log(gallery);
+}
+
 const sendForm = async () => {
-    await axios.post('/api/send-form')
+    await axios.post('/api/send-form', {
+        gallery: gallery.value,
+    })
         .then((res) => {
             console.log(res);
         })
@@ -18,6 +50,16 @@ const sendForm = async () => {
         id="app"
         v-on:submit.prevent="sendForm"
     >
+
+        <file-pond
+            name="test"
+            ref="pond"
+            label-idle="Drop files here..."
+            v-bind:allow-multiple="true"
+            accepted-file-types="image/jpeg, image/png"
+            v-on:processfile="handleProcessFile"
+            v-on:init="handleFilePondInit"
+        />
 
 
         <p>
